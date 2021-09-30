a6c86ef548e47b9795b5c907351485dbc1e23000
joetainment <github@joetainment.com>
2021-09-30 01:32:25 -0700

    cleaned up main2 and main.md.php

--- billandbuck.com/main.md.php
+++ billandbuck.com/main.md.php
@@ -1,5 +1,11 @@
 <?php
-//php tag about must start at very first character in file
+// php tag above must start at very first character in file
+// for header to work
+
+// This file only exists so that old bookmarks pointing
+// to main.md.php will still work.
+
+
 header("Location: ./main.php");
 die();
 
